6aeb59125705f06cdd467cf0a0d755a091788840
linuxlover6509 <badshodam11@gmail.com>
2025-10-22 00:27:33 +0300

    disk temperature update

--- monitoring/Hardware/hardwareTemp.sh
+++ monitoring/Hardware/hardwareTemp.sh
@@ -5,9 +5,27 @@ tempCPU(){
 	ZONE="${ZONE%/type}/temp"
 	TEMP=$(expr $(cat $ZONE) / 1000)
 
-	echo "CPU: $(awk -F': ' '/model name/ {print $2; exit}' /proc/cpuinfo) | TEMP: $TEMP°C"
+	echo "$(awk -F': ' '/model name/ {print $2; exit}' /proc/cpuinfo) | TEMP: $TEMP°C"
 }
 
-tempCPU
+tempDisk(){
+mapfile -t disk_array < <(cat /sys/class/hwmon/hwmon*/device/model)
+i=1
 
+for DISK in "${disk_array[@]}"; do
+	DPATH=$(grep -iRl "$DISK" /sys/class/hwmon/hwmon*/device/model)
+	DPATH="${DPATH/device\/model/temp1_input}"
 
+	DTEMP=$(expr $(cat $DPATH) / 1000)
+
+	echo "$i. $DISK | Temp: $DTEMP"
+
+	i=$(($i+1))
+done
+}
+
+echo "CPU:"
+echo "$(tempCPU)"
+echo "--------------------------------------------------------------"
+echo "Disks:"
+echo "$(tempDisk)"
